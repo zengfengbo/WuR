076fe8f9032fd198af2acdd8670ef5afdeff442d
zengfengbo <fengbo.zeng@gmail.com>
2017-12-17 19:57:52 +0800

    complete p 11

--- R/p11-multiple-regression.R
+++ R/p11-multiple-regression.R
@@ -1,22 +1,39 @@
-#' example 11.1 SC.csv
+#' multiple-regression
 #'
-#' @export
-f11.1 <- function()
-{
-}
-
-#' example 11.3
+#' @name p11-multiple-regression
+#' @examples
+#' # 11.1 模型和拟合
+#' # 例 11.1
+#' w = read.csv("SC.csv") # 没找到数据
+#' a = lm(营业利润~., w)
+#' summary(a); shapiro.test(a$res)
 #'
-#' @export
+#' # 11.2 变换及逐步回归
+#' a1 = lm(营业利润^0.75 ~., w)
+#' b = step(a1)
+#' summary(b); shapiro.test(b$res)
 #'
-f11.3 <- function(){
-  #w <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
-  w <- d11.3
-  a = glm(admit ~., w, family = binomial);
-  print(summary(a))
-  b = glm(admit~., w, family = binomial(probit))
-  print(summary(b))
-}
+#' # 11.3 自变量包含分类变量的回归
+#'
+#' # 例 11.2
+#' # w = read.csv("greenhouse2.csv")
+#' data(greenhouse, package="agricolae")
+#' w = greenhouse$greenhouse2
+#' a = lm(weight~. , w[, -3])
+#' summary(a) #method3个水平, variety4个水平, 默认第一个水平为0, 其他水平相对于第一个水平格式多少
+#' anova(a) #方差分析, F分布检测
+#'
+#' # 11.5 logistic回归和probit回归
+#' # 依然是线性回归, 但是左边的值取值为概率, 在[0, 1]范围内, 需要先映射到整个实数区间.
+#' # ln(p/(1-p)), 正态分布累积函数的逆.
+#'
+#' w <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
+#' #w <- d11.3
+#' a = glm(admit ~., w, family = binomial);
+#' print(summary(a))
+#' b = glm(admit~., w, family = binomial(probit))
+#' print(summary(b))
+NULL
 
 #' binary
 #'
